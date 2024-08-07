201e8e829fea9c6f96dada6523b59ba44f55bbf7
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2024-08-07 15:00:35 +0200

    refactor: icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()],
   build: {
     assets: "_assets",
   },
