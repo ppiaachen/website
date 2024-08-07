62bd82047624138e37b2a43a878582b89f662b34
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2024-08-07 15:01:16 +0200

    perf: minify build output

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@ import { defineConfig } from "astro/config";
 import { loadEnv } from "vite";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import playformCompress from "@playform/compress";
 
 const { SITE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [tailwind(), icon()],
+  integrations: [tailwind(), icon(), playformCompress()],
   build: {
     assets: "_assets",
   },
