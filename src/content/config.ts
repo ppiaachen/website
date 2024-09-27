6b74b92713af47f8aef21a88a999f8fae66e1d45
Hansen Frenico <16384641+frenicohansen@users.noreply.github.com>
2024-09-27 16:35:29 +0200

    refactor: use new content layer for gallery

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,55 @@
 import { defineCollection, z } from "astro:content";
-import { glob } from 'astro/loaders';
+import { glob } from "astro/loaders";
+
+const gallery = defineCollection({
+  loader: async () => {
+    let images = import.meta.glob<{ default: ImageMetadata }>(
+      "src/data/gallery/**/*.{jpeg,jpg}",
+    );
+
+    const resolvedImages = (await Promise.all(
+      Object.values(images).map((image) => image().then((mod) => mod.default)),
+    )).sort(() => Math.random() - 0.5);
+
+    let galleryInfo = import.meta.glob<{
+      default: {
+        pattern: string;
+        title: string;
+        eventUrl: string;
+      }[]
+    }>(
+      "src/data/gallery/metadata.json"
+    );
+
+    const resolvedInfo = (await Promise.all(
+      Object.values(galleryInfo).map((info) => info().then((mod) => mod.default)),
+    ))[0]
+
+    return resolvedImages.map((image) => {
+      const filename = image.src.split("/").pop() ?? "";
+      const info = resolvedInfo.find(
+        (info) => new RegExp(info.pattern).test(filename)
+      );
+      const imageUrl = 'src/' + image.src.split('?')[0].split('src/').pop();
+
+      return {
+        id: imageUrl,
+        title: info?.title ?? "",
+        eventUrl: info?.eventUrl ?? "",
+        image: imageUrl,
+      }
+    });
+  },
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      eventUrl: z.string(),
+      image: image(),
+    }),
+});
 
 const events = defineCollection({
-  loader: glob({ pattern: '**\/[^_]*.md', base: "./src/data/events" }),
+  loader: glob({ pattern: "**/[^_]*.md", base: "src/data/events" }),
   schema: ({ image }) =>
     z.object({
       title: z.string(),
@@ -15,10 +62,10 @@ const events = defineCollection({
 });
 
 const faq = defineCollection({
-  loader: glob({ pattern: '**\/[^_]*.md', base: "./src/data/faq" }),
+  loader: glob({ pattern: "**/[^_]*.md", base: "src/data/faq" }),
   schema: z.object({
     question: z.string(),
   }),
 });
 
-export const collections = { events, faq };
+export const collections = { events, faq, gallery };
